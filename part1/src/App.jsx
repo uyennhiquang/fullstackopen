0d466a69e7dc4b8b413658abe9c150d47cdd956d
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2023-09-03 13:32:09 -0400

    Refactor course parts variables into a single object Instead of having separate variables for each part, the code has been refactored to be one object, containing the course's name, andparts (an array containing each part as an object).

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -31,11 +31,11 @@ const Content = (props) => {
       {/* <p>{props.part1} {props.exercise1}</p> */}
       {/* <p>{props.part2} {props.exercise2}</p> */}
       {/* <p>{props.part3} {props.exercise3}</p> */}
-      
+
       {/* Component-based */}
-      <Part name={props.part1} exercises={props.exercises1} />
-      <Part name={props.part2} exercises={props.exercises2} />
-      <Part name={props.part3} exercises={props.exercises3} />
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
     </div>
   );
 };
@@ -50,8 +50,6 @@ const Total = (props) => {
 
 //
 const App = () => {
-  const course = "Half Stack application development";
-
   // const part1 = 'Fundamentals of React'
   // const exercises1 = 10
   // const part2 = 'Using props to pass data'
@@ -64,9 +62,14 @@ const App = () => {
   const part2 = partCreation("Using props to pass data", 7);
   const part3 = partCreation("State of a component", 14);
 
+  const course = {
+    name: "Half Stack application development",
+    parts: [part1, part2, part3],
+  };
+
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
 
       {/* Variable-based */}
       {/* <Content part1={part1} exercise1={exercises1} part2={part2} exercise2={exercises2} part3={part3} exercise3={exercises3}/> */}
@@ -75,18 +78,22 @@ const App = () => {
       {/* <Content part1Name={part1.name} part1Exercises={part1.exercises} part2Name={part2.name} part2Exercises={part2.exercises} part3Name={part3.name} part3Exercises={part3.exercises}/> */}
 
       {/* Component-based */}
-      { /* The Content tag's attribute pairs contain data for the Content component to have access to */}
-      <Content
-        part1={part1.name}
-        exercises1={part1.exercises}
-        part2={part2.name}
-        exercises2={part2.exercises}
-        part3={part3.name}
-        exercises3={part3.exercises}
-      />
+      {/* The Content tag's attribute pairs contain data for the Content component to have access to */}
+      {/* <Content
+        part1={parts[0].name}
+        exercises1={parts[0].exercises}
+        part2={parts[1].name}
+        exercises2={parts[1].exercises}
+        part3={parts[2].name}
+        exercises3={parts[2].exercises}
+  /> */}
+
+      <Content parts={course.parts} />
 
       <Total
-        totalExercises={part1.exercises + part2.exercises + part3.exercises}
+        totalExercises={course.parts.reduce((total, part) => {
+          return total + part.exercises;
+        }, 0)}
       />
     </div>
   );
