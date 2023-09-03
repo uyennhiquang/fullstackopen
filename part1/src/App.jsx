887a2c2c85dbb17ca5632f1a7cc89dc2e1b86abd
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2023-09-03 10:31:42 -0400

    Add webpage's reference image

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,15 @@
+const Header = () => {
+  return ();
+} 
+
+const Content = () => {
+  return ();
+}
+
+const Total  = () => {
+  return ();
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +21,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content />
+      <Total />
     </div>
   )
 }
