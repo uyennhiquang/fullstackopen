4ef17a511d4ce120db9070b54aa85d2555698f30
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2023-10-08 15:03:31 -0400

    Add an increment function for good feedback

--- part1/1-6_1-14/src/App.jsx
+++ part1/1-6_1-14/src/App.jsx
@@ -1,21 +1,42 @@
-import { useState } from 'react'
+'use strict'
+
+import { useState } from "react";
 
 const Display = (props) => {
-  return (<div>{props.title} {props.value}</div>)
-}
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
   return (
     <div>
-      <Display title="good" value={good}/>
-      <Display title="neutral" value={neutral}/>
-      <Display title="bad" value={bad}/>
+      {props.title} {props.value}
     </div>
-  )
-}
+  );
+};
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-export default App
+  const addGoodFeedback = () => {
+    const newFeedback = good + 1
+    setGood(newFeedback)
+  }
+
+  return (
+    <>
+      <section>
+        <h1>Give feedback</h1>
+        {/* <Button title="good" handleClick={addGoodFeedback} /> */}
+        <button onClick={addGoodFeedback}>good</button>
+        <button>neutral</button>
+        <button>bad</button>
+      </section>
+      <section>
+        <h1>statistics</h1>
+        <Display title="good" value={good} />
+        <Display title="neutral" value={neutral} />
+        <Display title="bad" value={bad} />
+      </section>
+    </>
+  );
+};
+
+export default App;
