07bda690d0e92bf25e477d2260ca503536af6e04
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2023-10-10 14:35:28 -0400

    cleaning

--- part1/1-6_1-14/src/App.jsx
+++ part1/1-6_1-14/src/App.jsx
@@ -55,7 +55,6 @@ const App = () => {
     <>
       <section>
         <h1>Give feedback</h1>
-        {/* <Button text="good" handleClick={addGoodFeedback} /> */}
         <Button text="good" handleClick={addFeedback("good")} />
 
         <Button text="neutral" handleClick={addFeedback("neutral")} />
