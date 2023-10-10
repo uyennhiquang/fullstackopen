764650ee1218873ac0579cf341cb6cf2c63c5c21
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2023-10-10 16:51:34 -0400

    thing random select and another vote through array

--- part1/1-6_1-14/src/App.jsx
+++ part1/1-6_1-14/src/App.jsx
@@ -2,112 +2,41 @@
 
 import { useState } from "react";
 
-const Button = (props) => {
-  return <button onClick={props.handleClick}>{props.text}</button>;
-};
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-const Statistics = ({ data }) => {
-  if (data.total === 0) {
-    return <div>No feedback given</div>;
+  const anecdotesLen = anecdotes.length;
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotesLen).fill(0));
+
+  const assignNewSelected = () => {
+    const newSelected = Math.round(Math.random() * (anecdotesLen - 1));
+
+    setSelected(newSelected);
+  };
+
+  const addVote = (selected) => () => {
+    const copyArr = [...votes]
+    copyArr[selected] += 1
+    setVotes(copyArr)
   }
 
   return (
-    <>
-      <table>
-        <tr>
-          <td>good</td>
-          <td>{data.good}</td>
-        </tr>
-        <tr>
-          <td>neutral</td>
-          <td>{data.neutral}</td>
-        </tr>
-        <tr>
-          <td>bad</td>
-          <td>{data.bad}</td>
-        </tr>
-        <tr>
-          <td>all</td>
-          <td>{data.total}</td>
-        </tr>
-        <tr>
-          <td>average</td>
-          <td>{data.average}</td>
-        </tr>
-        <tr>
-          <td>positive</td>
-          <td>{data.positiveRatio + "%"}</td>
-        </tr>
-      </table>
-    </>
-  );
-};
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const [total, setTotal] = useState(0);
-  const [average, setAverage] = useState(0);
-  const [positiveRatio, setPositiveRatio] = useState(0);
-
-  const addFeedback = (feedback) => () => {
-    let newGood = good;
-    let newNeutral = neutral;
-    let newBad = bad;
-
-    if (feedback == "good") {
-      newGood += 1;
-      setGood(newGood);
-    } else if (feedback == "neutral") {
-      newNeutral += 1;
-      setNeutral(newNeutral);
-    } else if (feedback == "bad") {
-      newBad += 1;
-      setBad(newBad);
-    }
-
-    // Calculate total
-    const newTotal = newGood + newNeutral + newBad;
-    setTotal(newTotal);
-
-    // Calculate average (good: 1, neutral: 0, bad: -1)
-    const newAverage = (newGood * 1 + newNeutral * 0 + newBad * -1) / newTotal;
-    setAverage(newAverage);
-
-    // Calculate positive
-    const newPositiveRatio = (newGood / newTotal) * 100;
-    setPositiveRatio(newPositiveRatio);
-  };
-
-  /* Interestingly enough, when incremented directly, the updated good variable seems to be printed correctly outside of the "if" statement, meaning when the if statement is done running, the state variables are updated. */
-  // console.log("Updated good (incrementing directly)", good)
-  // setTotal(total + 1);
-  // console.log("Updated total", total)
-
-  // On first increment, this will yield 0/0. This is because the setting function uses the 2 state variables that were directly incremented inside, so at the time they wouldn't have been updated yet. If we want to update any variables that rely on our 3 main state variables, it seems that we can *technically* do it outside of the if statement, in theory. In practice, it seems that placing the updating function in the main function seems to cause the function to render "infinitely".
-  // In general, calling the state-updating function directly from the main component seems to cause problems (e.g updating infinitely).
-  // setPositiveRatio(1)
-
-  return (
-    <>
-      <section>
-        <h1>Give feedback</h1>
-        <Button text="good" handleClick={addFeedback("good")} />
-        <Button text="neutral" handleClick={addFeedback("neutral")} />
-        <Button text="bad" handleClick={addFeedback("bad")} />
-      </section>
-
-      <section>
-        <h1>statistics</h1>
-        {/* Is this *also* a good idea...? */}
-        <Statistics
-          data={{ good, neutral, bad, total, average, positiveRatio }}
-        />
-      </section>
-    </>
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={addVote(selected)}>vote</button>
+      <button onClick={assignNewSelected}>next anecdote</button>
+    </div>
   );
 };
 
