ef6c8ae2ef9af75206f87193c3b4f3c0b95ce437
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2023-10-10 14:14:57 -0400

    Refactor feedback incr, add total incr Instead of having separate incrementing functions for each feedback category, there will be a general addFeedback() that takes a feedback category as a parameter, and increment accordingly. Also, instead of having an updated variable, the incrementation is expressed as an expression evaluated directly inside the setting function's argument.

--- part1/1-6_1-14/src/App.jsx
+++ part1/1-6_1-14/src/App.jsx
@@ -1,10 +1,10 @@
-'use strict'
+"use strict";
 
 import { useState } from "react";
 
 const Button = (props) => {
-  return(<button onClick={props.handleClick}>{props.text}</button>)
-}
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
 
 const Display = (props) => {
   return (
@@ -18,34 +18,56 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  
-  const [total, setTotal] = useState(0)
 
+  const [total, setTotal] = useState(0);
+
+  console.log("total after (re-render)", total)
   // These 3 functions update state
-  const addGoodFeedback = () => {
-    const newFeedback = good + 1
-    setGood(newFeedback)
-    
-    const newTotal = newFeedback + neutral + bad
-    setTotal(newTotal)
-  }
+  // const addGoodFeedback = () => {
+    // const newFeedback = good + 1;
+    // setGood(newFeedback);
+// 
+    // const newTotal = newFeedback + neutral + bad;
+    // setTotal(newTotal);
+  // };
 
-  const addNeutralFeedback = () => {
-    const newFeedback = neutral + 1
-    setNeutral(newFeedback)
-    
-    const newTotal = good + newFeedback + bad
-    setTotal(newTotal)
-  }
+  // const addNeutralFeedback = () => {
+    // const newFeedback = neutral + 1;
+    // setNeutral(newFeedback);
+// 
+    // const newTotal = good + newFeedback + bad;
+    // setTotal(newTotal);
+  // };
 
-  const addBadFeedback = () => {
-    const newFeedback = bad + 1
-    setBad(newFeedback)
-    
-    const newTotal = good + neutral + newFeedback
-    setTotal(newTotal)
-  }
+  // const addBadFeedback = () => {
+  //   const newFeedback = bad + 1;
+  //   setBad(newFeedback);
 
+  //   const newTotal = good + neutral + newFeedback;
+  //   setTotal(newTotal);
+  // };
+
+  const addFeedback = (feedback) => () => {
+    if (feedback == "good") {
+      // const newFeedback = good + 1;
+      // setGood(newFeedback);
+
+      setGood(good + 1);
+    } else if (feedback == "neutral") {
+      // const newFeedback = neutral + 1;
+      // setNeutral(newFeedback);
+
+      setNeutral(neutral + 1);
+    } else if (feedback == "bad") {
+      // const newFeedback = bad + 1;
+      // setBad(newFeedback);
+
+      setBad(bad + 1);
+    }
+    console.log("total before", total)
+
+    setTotal(total + 1);
+  };
 
   // Calculate average
 
@@ -53,7 +75,9 @@ const App = () => {
     <>
       <section>
         <h1>Give feedback</h1>
-        <Button text="good" handleClick={addGoodFeedback} />
+        {/* <Button text="good" handleClick={addGoodFeedback} /> */}
+        <Button text="good" handleClick={addFeedback("good")} />
+
         <Button text="neutral" handleClick={addNeutralFeedback} />
         <Button text="bad" handleClick={addBadFeedback} />
       </section>
