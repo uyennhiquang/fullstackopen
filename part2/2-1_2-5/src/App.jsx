3c35edd8a85db1bab0c67b86cedd54b50f1d399f
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2023-11-25 21:10:39 -0500

    Make separate module

--- part2/2-1_2-5/src/App.jsx
+++ part2/2-1_2-5/src/App.jsx
@@ -1,72 +1,59 @@
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-    </>
-  );
-}
-
-const Header = ({ name }) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-    </div>
-  );
-};
-
-const Part = ({ name, exercises }) => {
-  return (
-    <div>
-      <p>
-        {name} {exercises}
-      </p>
-    </div>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Total number of exercises: {props.totalExercises}</p>
-    </div>
-  );
-};
+import Course from "./components/Course"
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />
-}
+  // return <Course course={courses} />;
+  return (
+    <>
+      {courses.map((course) => (
+        <Course course={course}/>
+      ))}
+    </>
+  )
+};
 
 export default App;
---
+++ part2/2-1_2-5/src/components/Course.jsx
@@ -0,0 +1,42 @@
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+const Header = ({ name }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <div>
+      <p>
+        {name} {exercises}
+      </p>
+    </div>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+      <strong>
+        total of {parts.reduce((total, part) => total + part.exercises, 0)}{" "}
+        exercises
+      </strong>
+    </div>
+  );
+};
+
+export default Course
