d34aaf8a0919abb0940b757ea38de7750e7c73f6
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2024-01-06 16:12:29 -0500

    Setup json-server

--- part2/2-6_2-11/src/App.jsx
+++ part2/2-6_2-11/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Filter = (props) => (
   <>
@@ -38,15 +39,16 @@ const Persons = ({ persons }) => (
 );
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Aramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [personFilter, setPersonFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   const isIn = (person, persons) => {
     for (let i = 0; i < persons.length; i++) {
