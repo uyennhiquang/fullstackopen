3ab432db49ec60101133deab4b369818f9f4b459
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2024-01-09 10:51:02 -0500

    Add weather API

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Country = ({ country }) => {
+  const weatherAPI = import.meta.env.VITE_OPEN_WEATHER;
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${weatherAPI}`)
+      .then(re => setWeather(re.data));
+  }, [])
+
+  if (!weather) {
+    return <></>
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <h2>{country.name.common}</h2>
+        <p>capital {country.capital}</p>
+      </section>
+      <section>
+        <h3>languages:</h3>
+        <ul>
+          {Object.values(country.languages).map((language) => (
+            <li key={language}>{language}</li>
+          ))}
+        </ul>
+        <img src={country.flags.png} />
+      </section>
+      <section>
+        <h3>Weather in {country.capital}</h3>
+        <p>temperature {(weather.main.temp - 273.15).toPrecision(3)} celsius</p>
+        <p>wind {weather.wind.speed} m/s</p>
+      </section>
     </>
-  )
-}
+  );
+};
+const Countries = ({ countries }) => {
+  if (countries.length > 10) {
+    return <p>Too many matches. Specify another filter</p>;
+  } else if (countries.length == 1) {
+    const theCountry = countries[0];
+    return <Country country={theCountry} />;
+  } else {
+    return countries.map((country) => (
+      <p key={country.name.common}>{country.name.common}</p>
+    ));
+  }
+};
 
-export default App
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [countries, setCountries] = useState(null);
+  const [matchedCountries, setMatchedCountries] = useState([]);
+
+  useEffect(() => {
+    console.log("fetching countries...");
+    axios
+      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then((re) => setCountries(re.data))
+      .catch((e) => console.log(e));
+  }, []);
+
+  if (!countries) {
+    return null;
+  }
+
+  const handleQuery = (e) => {
+    const newQuery = e.target.value;
+    setQuery(newQuery);
+    setMatchedCountries(
+      countries.filter((country) =>
+        country.name.common.toLowerCase().includes(newQuery)
+      )
+    );
+  };
+
+  return (
+    <div>
+      find countries <input value={query} onChange={handleQuery} />
+      <Countries countries={matchedCountries} />
+    </div>
+  );
+};
+
+export default App;
